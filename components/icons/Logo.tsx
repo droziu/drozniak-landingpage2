5e0df31e26af84620b11f4b0eef748c04a418fd0
droziu <drozniakstanislaw@gmail.com>
2025-09-24 19:56:32 +0200

    logo

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -3,9 +3,8 @@ import React from 'react';
 
 export const Logo: React.FC = () => (
     <img 
-        src="/images/DROZNIAK_LOGO.svg" 
+        src="/images/drozniak_yellow.svg" 
         alt="Drozniak Logo" 
         className="h-6 md:h-7 w-auto"
-        style={{ filter: 'brightness(0) saturate(100%) invert(90%) sepia(100%) saturate(2000%) hue-rotate(45deg) brightness(1.3) contrast(1)' }}
     />
 );
